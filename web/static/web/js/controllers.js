d23c6bb291d71d14d2591a64d92f0fec93be80ce
Grzegorz Lewandowski <grzlew@gmail.com>
2015-11-21 17:36:26 +0100

    Room sites reloads fotorama gallries

--- web/static/web/js/controllers.js
+++ web/static/web/js/controllers.js
@@ -32,31 +32,29 @@ appControllers.controller('GalleryController', ['$scope', function ($scope) {
 }]);
 
 appControllers.controller('PricingController', ['$scope', 'Room', function ($scope, Room) {
-    this.rooms = Room.query();
+    var controller= this;
+    controller.rooms = Room.query();
 
     $scope.rooms = this.rooms;
-    $scope.options = {width: '100%', height: 400, loop: true, keyboard: true, nav: 'thumbs'};
+    //$scope.options = {width: '100%', height: 400, loop: true, keyboard: true, nav: 'thumbs'};
 
-    var selectRoom = function (roomId) {
-        var currentRoom = this.rooms[roomId];
+    $scope.selectRoom = function (roomId) {
+        var currentRoom = controller.rooms[roomId];
         $scope.currentRoom = currentRoom;
 
-        var gallery = $("#room-fotorama");
-        gallery.fotorama({data: currentRoom.images});
-        gallery.fotorama.load({data: currentRoom.images});
-        };
-
-    $scope.init = function () {
-        //var gallery = $("#room-fotorama");
-        //gallery.fotorama();
+        if (controller.galleryFotorama === undefined) {
+            var $gallery = $("#room-fotorama");
+            var gallery = $gallery.fotorama({data: currentRoom.images});
+            controller.galleryFotorama = gallery.data('fotorama');
+        } else {
+            controller.galleryFotorama.load(currentRoom.images);
+        }
     };
-
-    $scope.selectRoom = selectRoom
 
 }]);
 
 appControllers.controller('ContactController', ['$scope', function ($scope) {
-
+x
 }]);
 
 appControllers.controller('NotFoundController', ['$scope', function ($scope) {
