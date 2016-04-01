a91a2a679a48e3364e95039ff98cb9c47dbe3a67
grzegorz lewandowski <glewandowski@atlassian.com>
2016-04-01 19:28:41 +0200

    added STATIC_ROOT for heroku

--- hostel_paslek/settings.py
+++ hostel_paslek/settings.py
@@ -106,6 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'web/static'
 
 # REST_FRAMEWORK = {
 #     Use Django's standard `django.contrib.auth` permissions,
