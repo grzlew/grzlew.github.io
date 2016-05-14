eab03ba92756b0e2848869cc26e5f29c2ed1a090
Grzegorz Lewandowski <grzlew@gmail.com>
2016-05-14 21:41:19 +0200

    Minor fixes
    Former-commit-id: 3a7ee6a6839bc1f3449b6c290cc4450f714b304a

--- web/static/web/js/i18n/locale_pl.js
+++ web/static/web/js/i18n/locale_pl.js
@@ -37,12 +37,12 @@ var messages_pl = {
     landing_welcome_text: '<p>W Pasłęku mieszkamy od 1986r. i wciąż pozostajemy pod urokiem tego miasta, jego mieszkańców, a także okolicznych atrakcji. Nasza fascynacja Pasłękiem i okolicami skłoniła nas do stworzenia miejsca w którym turyści mogliby poczuć atmosferę naszego niewielkiego, lecz tętniącego życiem miasteczka. Miejscem tym są Apartamenty KAMIENICA.</p>' +
     '<p>Położone w samym sercu starego miasta, Apartamenty KAMIENICA są doskonałą bazą wypadową dla turystów pragnących poznać historię i kulturę skraju Warmii, Żuław i Pojezierza Iławskiego. Odrestaurowana w 2016 roku kamienica ma służyć za spokojną przystań tym, którzy chcą zagrać w golfa na pobliskim polu Sand Valley, pojeździć konno w stadninie w Rzecznej, wybrać się na rejs po kanale Elbląskim, czy też posłuchać muzyki organowej w oddalonym o 50 metrów kościele św. Bartłomieja. Położenie Pasłęka pozwala również naszym gościom na łatwy i szybki dojazd do Gdańska, Elbląga i Olsztyna.</p>' +
     '<p>Zachęcamy Was do odwiedzenia Pasłęka i skorzystania z naszej oferty noclegowej</p>' +
-    '<div style="float:right"> <i>Lucyna i Paweł Chomniccy</i> </div>',
+    '<div style="float:right; margin-right:150px; margin-top:50px"> <i>Lucyna i Paweł Chomniccy</i> </div>',
     landing_offerings: '<p> Chcielbyśmy zaprosić Państwa do kamienicy wybudowanej w XIX w. poddanej generalnemu remontowi w roku 2016. Do dyspozycji' +
     'gości oferujemy:</p>  <ul>  ' +
     '<li>Trzy dwupokojowe apartamenty z łazienką – sypialnia dla 2 osób + dostawka w pokoju dziennym dla maksymalnie dwóch osób.  </li>' +
     '<li>Jeden dwuosobowy pokój z łazienką.</li>' +
-    '<li>Świetlica z aneksem kuchennym – ekspres do kawy, kuchenka mikrofalowa, lodówki, zmywarka, płyta indukcyjna. </li> </ul>',
+    '<li>Świetlica z aneksem kuchennym – ekspres do kawy, kuchenka mikrofalowa, lodówka, zmywarka, płyta indukcyjna. </li> </ul>',
     our_offerings: 'Co oferujemy?'
 
 };
