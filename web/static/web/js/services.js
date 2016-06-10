0c1d9c03110e2af2bcad43347c8184538897a01f
grzegorz lewandowski <glewandowski@atlassian.com>
2016-06-10 21:49:29 +0200

    imporoved localized services
    Former-commit-id: be13eec63e9e7bffc6786bab5d850a3a10265ce2

--- web/static/web/js/services.js
+++ web/static/web/js/services.js
@@ -1,12 +1,13 @@
 'use strict';
 
+var supportedLangs = ['pl', 'en'];
 var appServices = angular.module('appServices', ['ngResource']);
 
 appServices.factory('Room', ['$resource',
     function ($resource) {
         var lang = localStorage.getItem(lang_key);
 
-        if (lang !== 'en' && lang !== 'pl') {
+        if (supportedLangs.indexOf(lang) == -1) {
             lang = 'pl'
         }
         return $resource('/static/web/data/rooms_' + lang + '.json', {}, {
